6aef08cdde73ffa4acc03ce1583fd82a286d74d2
jnowak <jakub.nowak@placewise.com>
2022-02-28 15:52:19 +0100

    after rubocop refactor

--- app/api/verification.rb
+++ app/api/verification.rb
@@ -1,39 +1,40 @@
+# frozen_string_literal: true
+
 require 'grape'
 require 'time'
 require 'credit/verificator'
 require_relative 'postgres_connector'
 
 module API
-   class Verification < Grape::API
-        #@@verificator = Credit::Verificator.new
-        format :json
-        params do
-         optional :id, type: Integer
-         optional :code, type: String
-         optional :owner, type: String
-        end
+  class Verification < Grape::API
+    # @@verificator = Credit::Verificator.new
+    format :json
+    params do
+      optional :id, type: Integer
+      optional :code, type: String
+      optional :owner, type: String
+    end
 
-         get :card do
-            {card: Credit::Card.new(params[:id], params[:code], params[:owner])}
-         end
+    get :card do
+      { card: Credit::Card.new(params[:id], params[:code], params[:owner]) }
+    end
 
-         get :verify do
-            db_handler = API::DBHandler.new(:verifications)
-            ver = Credit::CardVerificator.new
-            card = Credit::Card.new(params[:id], params[:code], params[:owner])
-            result = ver.verify(card)
+    get :verify do
+      db_handler = API::DBHandler.new(:verifications)
+      ver = Credit::CardVerificator.new
+      card = Credit::Card.new(params[:id], params[:code], params[:owner])
+      result = ver.verify(card)
 
-            card = {:card_code => card.code,
-                    :verified_as => result,
-                    :verified_at => Time.now.getutc}
-            query = db_handler.insert_card(card)
-            query.nil? ? "NIL" : query
-            end
+      card = { card_code: card.code,
+               verified_as: result,
+               verified_at: Time.now.getutc }
+      query = db_handler.insert_card(card)
+      query.nil? ? 'NIL' : query
+    end
 
-         get :all do
-            db_handler = API::DBHandler.new(:verifications)
-            db_handler.get_all()
-         end
-
-   end
+    get :all do
+      db_handler = API::DBHandler.new(:verifications)
+      db_handler.get_all
+    end
+  end
 end
